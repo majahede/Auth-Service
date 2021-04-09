78304f1e60fdc0a5cc312880da8c426e04da0bbd
Maja Hedegard <mh223pi@student.lnu.se>
2021-04-09 12:00:04 +0200

    add user-router

---
+++ src/routes/api/v1/user-router.js
@@ -0,0 +1,17 @@
+/**
+ * API version 1 routes.
+ *
+ * @author Maja Hedegärd.
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { UserController } from '../../../controllers/api/user-controller.js'
+
+export const router = express.Router()
+
+const controller = new UserController()
+
+router.post('/register', (req, res, next) => controller.register(req, res, next))
+
+router.post('/login', (req, res, next) => controller.login(req, res, next))
