22ff515228ae2a95b9c2c3105816fe4c5f59ae07
Maja Hedegard <mh223pi@student.lnu.se>
2021-04-09 15:34:35 +0200

    add login

--- src/controllers/api/account-controller.js
+++ src/controllers/api/account-controller.js
@@ -4,9 +4,9 @@
  * @author Maja Hedegärd
  * @version 1.0.0
  */
-// import createError from 'http-errors'
-// import { User } from '../../models/user.js'
-// import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+import { User } from '../../models/user.js'
+import jwt from 'jsonwebtoken'
 
 /**
  * Encapsulates a controller.
@@ -36,9 +36,25 @@ export class AccountController {
    */
   async login (req, res, next) {
     try {
-      //
+      const user = await User.authenticate(req.body.email, req.body.password)
+      const payload = {
+        email: user.email,
+        permission_level: user.permissionLevel
+      }
+
+      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        algorithm: 'HS256',
+        expiresIn: '1h'
+      })
+      res
+        .status(201)
+        .json({
+          access_token: accessToken
+        })
     } catch (error) {
-      next(error)
+      const err = createError(401)
+      err.innerException = error
+      next(err)
     }
   }
 }
